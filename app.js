ec2d43849a0e14efb88aa0462487a3f00e21eedf
David Charbonnier <d.charbonnier@oxys.net>
2011-09-20 20:17:08 +0200

    hello world

---
+++ app.js
@@ -0,0 +1,8 @@
+var http = require('http');
+
+module.exports = http.createServer(function(req, res){
+  console.log('%s %s', req.method, req.url);
+  var body = 'Hello World';
+  res.writeHead(200, { 'Content-Length': body.length });
+  res.end(body);
+});
---
+++ server.js
@@ -0,0 +1,9 @@
+var cluster = require('cluster');
+
+cluster('./app')
+  .use(cluster.logger('logs'))
+  .use(cluster.stats())
+  .use(cluster.pidfiles('pids'))
+  .use(cluster.cli())
+  .use(cluster.repl(8888))
+  .listen(3000);
