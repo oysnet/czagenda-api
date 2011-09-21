6fb5915abe571bf27e10d166968ad8c8774b3cbe
David Charbonnier <d.charbonnier@oxys.net>
2011-09-21 13:52:53 +0200

    spawn master process

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
-var cluster = require('cluster');
+var cluster = require('cluster'),
+    start = process.argv[process.argv.length - 1] === 'start';
+
+if (start) {
+    var node = process.execPath,
+        cmd = process.argv.slice(1, -1);
+
+    spawn(node, cmd, { env : process.env, setsid: true });
+    process.exit(0);
+}
+
 
 cluster('./app')
   .use(cluster.logger('logs'))
