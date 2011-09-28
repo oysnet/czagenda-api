4a967527fea0c0061239acb81c0129a187ca398e
David Charbonnier <d.charbonnier@oxys.net>
2011-09-28 12:14:40 +0200

    test

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var http = require('http');
-
+config = require('./config');
 
 module.exports = http.createServer(function(req, res){
   console.log('%s %s', req.method, req.url);
-  var body = 'Hello World 2';
+  var body = 'Hello World '+config.IP;
   res.writeHead(200, { 'Content-Length': body.length });
   res.end(body);
 });
