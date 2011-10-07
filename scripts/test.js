1bbaa8f3b337e8c45f89cdb47b9fe1d7e7ebe3fa
Jean-Hugues Pinson <jh.pinson@gmail.com>
2011-10-07 10:09:43 +0200

    several minor things

--- scripts/test.js
+++ scripts/test.js
@@ -1,69 +1,20 @@
-/*var Event = require('./models/event.js').Event;
-var o = {};
-var e = new Event()
-e.event = {'title' : 'Mon evenement'};
-e.author = 'Moi';
-e.save(function (err, obj) {o.o = obj})
 
+var settings = require('../settings');
 
-*/
-
-var Agenda = require('./models/agenda.js').Agenda;
-var o = {};
-var e = new Agenda()
-e.title = 'Mon agenda Perso';
-e.description = 'L\'agenda personnel de Jean-Hugues Pinson'
-e.save(function (err, obj) {o.o = obj})
-
-
-var Group = require('./models/group.js').Group;
-var o = {};
-var e = new Group()
-e.title = 'Mon agenda Perso';
-e.description = 'L\'agenda personnel de Jean-Hugues Pinson'
-e.save(function (err, obj) {o.o = obj})
-
-var User = require('./models/user.js').User;
-var o = {};
-var e = new User()
-e.login = 'jhpinson'
-e.first_name = 'Jean-Hugues'
-e.last_name = 'Pinson';
-e.password = 'eeeeee'
-e.email = 'jh.pinson@gmail.com'
-e.save(function (err, obj) {o.o = obj})
-
-var User = require('./models/user.js').User;
-var o = {};
-var e = new User()
-e.login = 'isabelle'
-e.first_name = 'Isabelle'
-e.last_name = 'Pinson';
-e.password = 'eeeeee'
-e.email = 'i.pinson@gmail.com'
-e.save(function (err, obj) {o.o = obj})
-
-
-var User = require('./models/user.js').User;
-var o = {};
-var e = new User()
-e.login = 'louise'
-e.first_name = 'louise'
-e.last_name = 'Pinson';
-e.password = 'eeeeee'
-e.email = 'l.pinson@gmail.com'
-e.save(function (err, obj) {o.o = obj})
-
-var settings = require('../settings.js');
-var ElasticSearchClient = require('elasticSearchClient');
-var elasticSearchClient = new ElasticSearchClient(settings.elasticsearch);
-
-var q = elasticSearchClient.deleteDocument('agenda', 'event', 'test');
-console.log('1')
-
-q.on('data', function() {
-	console.log(arguments);
-});
-console.log('2')
-q.exec();
-console.log('3')
+var OAuth= require('node-oauth').OAuth;
+var oa= new OAuth("http://term.ie/oauth/example/request_token.php",
+"http://term.ie/oauth/example/access_token.php",settings.oauth.consumer_token,settings.oauth.consumer_secret,"1.0",null,"HMAC-SHA1")
+      
+    
+oa.put("http://localhost:3000/agenda/title-agenda-3",  settings.oauth.access_token,  
+	settings.oauth.access_secret, JSON.stringify({title:'test'}), 'application/json', 
+	function (error, data, response) {
+		console.log(data)
+	})
+      
+     /*   
+oa.getProtectedResource("http://localhost:3000/agenda", "GET", settings.oauth.access_token,  
+	settings.oauth.access_secret,
+	function (error, data, response) {
+          console.log(data);
+      });  */    
--- tools/dump_es.js
+++ tools/dump_es.js
@@ -1,8 +1,8 @@
-var Rest = require('../libs/rest-client').Rest,
+var Rest = require('../libs/rest-client-classic').Rest,
 	fs=require('fs');
 
 var rest = new Rest('10.7.50.110', 9200);
-rest.post('/agenda/_search',JSON.stringify({size:1000}),function(err, res, data) {
+rest.post('/agenda-dev/_search',JSON.stringify({size:1000}),function(err, res, data) {
 	
 	var data = JSON.parse(data),  dumps = [];
 	
@@ -12,7 +12,7 @@ rest.post('/agenda/_search',JSON.stringify({size:1000}),function(err, res, data)
 		
 	}
 	
-	  fs.writeFile('./dumps/es-dump.sh', dumps.join('\n'), function (err) {
+	  fs.writeFile('./dumps/agenda-dev.sh', dumps.join('\n'), function (err) {
 	  if (err) throw err;
 	  console.log('It\'s saved!');
 	});
