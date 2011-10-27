cb9280cfb3b7c49c8b5c8fbf12a93dbad6470a68
Jean-Hugues Pinson <jh.pinson@gmail.com>
2011-10-27 12:54:25 +0200

    schema creation

--- tools/createSchemas.js
+++ tools/createSchemas.js
@@ -4,82 +4,109 @@ var models = require('../models');
 var fs = require('fs');
 var log = require('czagenda-log').from(__filename);
 
-var SCHEMAS = [
-
-	 {
-		"name" : "base",
-		"final" : false,
-		"file" : "base.json"
-	},
-	{
-		"name" : "who",
-		"final" : true,
-		"file" : "who.json"
-	},
-	
-	{
-		"name" : "geo",
-		"final" : true,
-		"file" : "geo.json"
-	},
-	
-	{
-		"name" : "localization",
-		"final" : true,
-		"file" : "localization.json"
-	},
-	
-	{
-		"name" : "event",
-		"final" : true,
-		"file" : "event.json"
-	}
-	
-
-]
+var SCHEMAS = [{
+	"name" : "base",
+	"final" : false,
+	"file" : "base-abstract.json"
+}, {
+	"name" : "who",
+	"final" : false,
+	"file" : "who-abstract.json"
+}, {
+	"name" : "who",
+	"final" : true,
+	"file" : "who.json"
+}, {
+	"name" : "geo",
+	"final" : false,
+	"file" : "geo-abstract.json"
+}, {
+	"name" : "geo",
+	"final" : true,
+	"file" : "geo.json"
+}, {
+	"name" : "localization",
+	"final" : false,
+	"file" : "localization-abstract.json"
+}, {
+	"name" : "localization",
+	"final" : true,
+	"file" : "localization.json"
+}, {
+	"name" : "event",
+	"final" : false,
+	"file" : "event-abstract.json"
+}, {
+	"name" : "event",
+	"final" : true,
+	"file" : "event.json"
+}, {
+	"name" : "entity",
+	"final" : false,
+	"file" : "entity-abstract.json"
+}, {
+	"name" : "organization",
+	"final" : false,
+	"file" : "organization-abstract.json"
+}, {
+	"name" : "organization",
+	"final" : true,
+	"file" : "organization.json"
+}, {
+	"name" : "person",
+	"final" : false,
+	"file" : "person-abstract.json"
+}, {
+	"name" : "person",
+	"final" : true,
+	"file" : "person.json"
+}]
 
 var createSchema = function() {
 
+	var data = SCHEMAS.shift();
+
+	log.notice("Try to create " + data.name);
+
+	var schema = new models.Schema();
+	schema.name = data.name;
+	schema.author = "/user/jh.pinson";
+	schema['final'] = data['final'];
+	schema.status = 'APPROVED';
+	schema.schema = JSON.parse(fs.readFileSync("./schemas/" + data.file, 'utf8'));
+
+	schema.save(function(err, obj) {
 		
-		var data = SCHEMAS.shift();
 		
-		log.notice("Try to create " + data.name);
-		
-		var schema = new models.Schema();
-		schema.name = data.name;
-		schema.author = "/user/jh.pinson";
-		schema['final'] = data['final'];
-		schema.status = 'APPROVED';
-		schema.schema = JSON.parse(fs.readFileSync("./schemas/" + data.file, 'utf8'));
-		
-		
-		schema.save(function(err, obj) {
-			if(err === null) {
-				log.notice("Succesfully created " + data.name, obj.id);
-				
-				if (SCHEMAS.length > 0) {
-					setTimeout(createSchema, 1000);
-				} else {
-					log.notice("All done");
-				}
-				
+		if(err === null || err instanceof models.errors.ObjectAlreadyExists) {
+			
+			if( err instanceof models.errors.ObjectAlreadyExists) {
+				console.log('ObjectAlreadyExists', data.name);
+
 			} else {
+				log.notice("Succesfully created " + data.name, obj.id);
+			}
+			
+			
 
-				if( err instanceof models.errors.ObjectAlreadyExists) {
-					console.log('ObjectAlreadyExists', data.name);
-
-				} else if( err instanceof models.errors.ValidationError) {
-					console.log(obj.validationErrors, data.name);
-
-				} else {
-					console.log('Internal error', data.name)
-				}
+			if(SCHEMAS.length > 0) {
+				setTimeout(createSchema, 1000);
+			} else {
+				log.notice("All done");
 			}
 
-		});
-	}
+		} else {
 
+			 if( err instanceof models.errors.ValidationError) {
+				console.log(obj.validationErrors, data.name);
 
+			} else {
+				console.log('Internal error', data.name)
+			}
+		}
+
+	});
+}
 
 approvedEnvironment.load(function(err, success) {
 	createSchema();
