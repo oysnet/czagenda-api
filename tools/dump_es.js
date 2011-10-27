e0a3faa2359a5e46ae0631e3abef75fa9f2a5b24
Jean-Hugues Pinson <jh.pinson@gmail.com>
2011-10-27 12:51:56 +0200

    clean

--- tools/dump_es.js
+++ tools/dump_es.js
@@ -9,7 +9,7 @@ rest.post('/agenda-test/_search',JSON.stringify({size:1000}),function(err, res, 
 	for (var i = 0, l = data.hits.hits.length; i<l; i++) {
 		var d = data.hits.hits[i];
 		
-		console.log(typeof(d._source))
+		
 		
 		dumps.push("curl -XPUT http://10.7.50.110:9200/" + d._index + "/" + d._type + "/" + encodeURIComponent(d._id) + " -d '" + JSON.stringify(d._source) + "';");
 		
