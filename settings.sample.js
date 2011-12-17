b299d3c944a81cd6f675930cd0e847faefd14332
Jean-Hugues Pinson <jh.pinson@oxys.net>
2011-12-17 10:45:21 +0100

    add %ES_IP% and %REDIS_IP%

--- settings.sample.js
+++ settings.sample.js
@@ -7,7 +7,7 @@ exports.czdiscovery = {
 exports.elasticsearch = {
 	index : 'agenda',
 	hosts : [{
-		'host' : '10.7.100.2',
+		'host' : '%ES_IP%',
 		'port' : 9200
 	}]
 }
@@ -18,7 +18,7 @@ exports.server = {
 }
 
 exports.redis = {
-	'host' : '10.7.100.3',
+	'host' : '%REDIS_IP%',
 	'port' : 6379,
 	'keyTTL' : 3600,
 	'db' : 0
