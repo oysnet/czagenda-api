c8e5c6deefef891796e5a5f7e58eff1fff6ca104
Jean-Hugues Pinson <jh.pinson@oxys.net>
2011-12-20 17:19:27 +0100

    tools to create consumer/token

--- tools/createOAuthTokens.js
+++ tools/createOAuthTokens.js
@@ -4,17 +4,23 @@ var models = require('../models');
 
 var user = process.argv[2];
 
-
+var consumerToken = null;
+var consumerSecret = null;
+var token = null;
+var secret = null;
 
 var consumer = new models.OAuthConsumer();
 consumer.user = user;
 consumer.name = process.argv[3];
-consumer.description = process.argv[3];
+consumer.description = process.argv[4];
 
 consumer.save(function(err, obj) {
 	if(err === null) {
-		console.log(obj.serialize(['id', 'name', 'key', 'secret']))
-
+		//console.log(obj.serialize(['id', 'name', 'key', 'secret']))
+		
+		consumerToken = obj.key;
+		consumerSecret = obj.secret;
+		
 		var token = new models.OAuthToken();
 		token.consumer = obj.id;
 		token.user = user;
@@ -24,6 +30,15 @@ consumer.save(function(err, obj) {
 		token.save(function(err, obj) {
 			if(err === null) {
 				console.log(obj.serialize(['id', 'key', 'secret']))
+				
+				token = obj.key;
+				secret = obj.secret;
+				
+				console.log("Consumer token: " + consumerToken);
+				console.log("Consumer secret: " + consumerSecret);
+				console.log("Access token: " + token);
+				console.log("Access token: " + secret);
+				
 			} else {
 
 				if( err instanceof models.errors.ObjectAlreadyExists) {
