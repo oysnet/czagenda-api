4c58e5def1aff67326c277b98f765853a39ec722
Jean-Hugues Pinson <jh.pinson@oxys.net>
2012-03-16 15:15:47 +0100

    login is not editable

--- models/user.js
+++ models/user.js
@@ -23,15 +23,20 @@ function User() {
 		gravatar : null
 	};
 	Base.call(this, 'user');
+	
+	this.initialData = {
+		login : null
+	}
+	
 }
 
 User.publicAttributes = Base.publicAttributes.concat(['login', 'firstName', 'lastName', 'isActive', 'isStaff', 'isSuperuser', 'lastSeen', 'joinedDate', 'groups', 'gravatar']);
 
 User.staffAttributes = User.publicAttributes.concat(Base.staffAttributes).concat(['email', 'password']);
 
-User.publicWriteAttributes = ['login', 'firstName', 'lastName', 'email',  'isActive', 'password'];
+User.publicWriteAttributes = ['firstName', 'lastName', 'email',  'isActive', 'password'];
 
-User.staffWriteAttributes = User.publicWriteAttributes.concat([ 'isStaff', 'isSuperuser']);
+User.staffWriteAttributes = User.publicWriteAttributes.concat([ 'isStaff', 'isSuperuser', 'login']);
 
 
 util.inherits(User, Base);
@@ -59,7 +64,19 @@ User.prototype.hasPerm = function (perm, user, callback) {
 
 User.prototype._validate = function(callback) {
 	//this.validateString('login', false, 2, 30);
-	this.validateRegexp('login', '^[\-_\.0-9a-zA-Z]{2,30}$', false);
+	
+	if (this.initialData.login !== null) {
+		
+		if (this.initialData.login !== this.login) {
+			this.addValidationError('login', 'is not editable');
+		}
+		
+	} 
+	else {
+		this.validateRegexp('login', '^[\-_\.0-9a-zA-Z]{2,30}$', false);
+	}
+	
+	
 	this.validateString('firstName', true, null, 128);
 	this.validateString('lastName', true, null, 128);
 	this.validateEmail('email');
@@ -135,11 +152,25 @@ User.prototype._postDel = function(err, next) {
 	
 }
 
+User.get = function(options, callback) {
+	Base.get(options, User, function(err, obj) {
 
+		// save initial agenda value to check perms
+		if(err === null) {
+			obj.initialData = {
+				login : obj.login
+			}
+		}
+
+		callback(err, obj);
+
+	})
+}
+/*
 User.get = function(options, callback) {
 	Base.get(options, User, callback)
 }
-
+*/
 User.search = function(query, attrs, callback) {
 	Base.search(query, settings.elasticsearch.index, 'user', attrs, User, callback)
 }
